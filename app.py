3412c20a47b196034e1bfadbad2b22b2eece7267
dheeraj009joshi <73873399+dheeraj009joshi@users.noreply.github.com>
2024-07-13 14:22:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import openai
-import config
 
 # Load environment variables
 load_dotenv()

f5077ca7c40f226f95ef210a5daadb12f3100fe8
dheeraj009joshi <73873399+dheeraj009joshi@users.noreply.github.com>
2024-07-14 20:20:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import openai
 load_dotenv()
 
 # Set up the OpenAI API key
-openai_api_key = "sk-proj-mAIJbXohFjyF2hAYFdLCT3BlbkFJwtih77YxvVeIrk8y5jna"  # Update your .env file with OPENAI_API_KEY
+openai_api_key = "sk-proj-aNNfNbMDDTZAw8jGTDnET3BlbkFJAquDOf8lrwHva61niNsd"  # Update your .env file with OPENAI_API_KEY
 openai.api_key = openai_api_key
 
 # Constants
